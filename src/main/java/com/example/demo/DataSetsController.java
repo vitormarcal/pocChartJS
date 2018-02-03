018cbcd1433cff13d2c0497bdb73355d55a7e111
Vítor Marçal <vitormarcal8@gmail.com>
2018-02-03 17:45:19 -0200

    refatoracão

--- src/main/java/com/example/demo/DataSetsController.java
+++ src/main/java/com/example/demo/DataSetsController.java
@@ -12,28 +12,50 @@ import java.util.Random;
 @RequestMapping(value = "/veiculos")
 public class DataSetsController {
 
-    @GetMapping("/countQuantidadeVeiculosMes")
-    public DataSetDTO countQuantidadeVeiculosMes() {
+    @GetMapping("/countQuantidadeVeiculosCompradosMes")
+    public DataSetDTO countQuantidadeVeiculosCompradosMes() {
+        DataSetDTO dataSetDTO = populaDataSet(gerarValores(), "Veiculos Comprados");
+        dataSetDTO.setBackgroundColor(0, "rgba(255,  99, 132, 0.2)");
+        dataSetDTO.setBackgroundColor(1, "rgba( 54, 162, 235, 0.2)");
+        dataSetDTO.setBackgroundColor(2, "rgba(255, 206,  86, 0.2)");
+        dataSetDTO.setBackgroundColor(3, "rgba( 75, 192, 192, 0.2)");
+        dataSetDTO.setBackgroundColor(4, "rgba(153, 102, 255, 0.2)");
+        dataSetDTO.setBackgroundColor(5, "rgba(255, 159,  64, 0.2)");
 
-        final Map<Integer, Integer> resultadoBanco = gerarValores();
-        String label = "Veiculos";
+        dataSetDTO.setBorderColor(0, "rgba(255,  99, 132, 1)");
+        dataSetDTO.setBorderColor(1, "rgba( 54, 162, 235, 1)");
+        dataSetDTO.setBorderColor(2, "rgba(255, 206,  86, 1)");
+        dataSetDTO.setBorderColor(3, "rgba( 75, 192, 192, 1)");
+        dataSetDTO.setBorderColor(4, "rgba(153, 102, 255, 1)");
+        dataSetDTO.setBorderColor(5, "rgba(255, 159,  64, 1)");
+        return dataSetDTO;
+    }
+
+    @GetMapping("/countQuantidadeVeiculosVendidosMes")
+    public DataSetDTO countQuantidadeVeiculosVendidosMes() {
+
+        DataSetDTO dataSetDTO = populaDataSet(gerarValores(), "Veiculos vendidos");
+        dataSetDTO.setBackgroundColor(0, "rgba(220, 199,  32, 0.2)");
+        dataSetDTO.setBackgroundColor(1, "rgba( 30, 102, 135, 0.2)");
+        dataSetDTO.setBackgroundColor(2, "rgba(255,  26, 126, 0.2)");
+        dataSetDTO.setBackgroundColor(3, "rgba(  8, 129, 210, 0.2)");
+        dataSetDTO.setBackgroundColor(4, "rgba(123, 122, 200, 0.2)");
+        dataSetDTO.setBackgroundColor(5, "rgba(127, 195, 246, 0.2)");
+
+        dataSetDTO.setBorderColor(0, "rgba(220, 199,  32, 1)");
+        dataSetDTO.setBorderColor(1, "rgba( 30, 102, 135, 1)");
+        dataSetDTO.setBorderColor(2, "rgba(255,  26, 126, 1)");
+        dataSetDTO.setBorderColor(3, "rgba(  8, 129, 210, 1)");
+        dataSetDTO.setBorderColor(4, "rgba(123, 122, 200, 1)");
+        dataSetDTO.setBorderColor(5, "rgba(127, 195, 246, 1)");
+        return dataSetDTO;
+    }
+
+    private DataSetDTO populaDataSet(final Map<Integer, Integer> resultadoBanco, final String label) {
         DataSetDTO dataSetDTO = new DataSetDTO(label, 12);
         for (Map.Entry<Integer, Integer> pair : resultadoBanco.entrySet()){
-                dataSetDTO.set(pair.getKey(), pair.getValue());
+            dataSetDTO.set(pair.getKey(), pair.getValue());
         }
-        dataSetDTO.setBackgroundColor(0, "rgba(255, 99, 132, 0.2)");
-        dataSetDTO.setBackgroundColor(1, "rgba(54, 162, 235, 0.2)");
-        dataSetDTO.setBackgroundColor(2, "rgba(255, 206, 86, 0.2)");
-        dataSetDTO.setBackgroundColor(3, "rgba(75, 192, 192, 0.2)");
-        dataSetDTO.setBackgroundColor(4, "rgba(153, 102, 255, 0.2)");
-        dataSetDTO.setBackgroundColor(5, "rgba(255, 159, 64, 0.2)");
-
-        dataSetDTO.setBorderColor(0, "rgba(255, 99, 132, 1)");
-        dataSetDTO.setBorderColor(1, "rgba(54, 162, 235, 1)");
-        dataSetDTO.setBorderColor(2, "rgba(255, 206, 86, 1)");
-        dataSetDTO.setBorderColor(3, "rgba(75, 192, 192, 1)");
-        dataSetDTO.setBorderColor(4, "rgba(153, 102, 255, 1)");
-        dataSetDTO.setBorderColor(5, "rgba(255, 159, 64, 1)");
         return dataSetDTO;
     }
 
--- src/main/resources/static/gerarGrafico.js
+++ src/main/resources/static/gerarGrafico.js
@@ -8,48 +8,6 @@
                 labels: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho',
                     'Julho','Agosto','Setembro','Outubro','Novembro', 'Dezembro'],
                 datasets: datasets
-                // datasets: [{
-                //     label: '# of Votes',
-                //     data: [12, 19, 90, 5, 20, 3],
-                //     backgroundColor: [
-                //         'rgba(255, 99, 132, 0.2)',
-                //         'rgba(54, 162, 235, 0.2)',
-                //         'rgba(255, 206, 86, 0.2)',
-                //         'rgba(75, 192, 192, 0.2)',
-                //         'rgba(153, 102, 255, 0.2)',
-                //         'rgba(255, 159, 64, 0.2)'
-                //     ],
-                //     borderColor: [
-                //         'rgba(255,99,132,1)',
-                //         'rgba(54, 162, 235, 1)',
-                //         'rgba(255, 206, 86, 1)',
-                //         'rgba(75, 192, 192, 1)',
-                //         'rgba(153, 102, 255, 1)',
-                //         'rgba(255, 159, 64, 1)'
-                //     ],
-                //     borderWidth: 1
-                // },
-                //     {
-                //         label: '# of csasa',
-                //         data: [8, 30, 15, 5, 29, 7],
-                //         backgroundColor: [
-                //             'rgba(220, 199, 32, 0.2)',
-                //             'rgba(30, 102, 135, 0.2)',
-                //             'rgba(255, 26, 126, 0.2)',
-                //             'rgba(8, 129, 210, 0.2)',
-                //             'rgba(123, 122, 200, 0.2)',
-                //             'rgba(127, 195, 246, 0.2)'
-                //         ],
-                //         borderColor: [
-                //             'rgba(220, 199, 32, 1)',
-                //             'rgba(30, 102, 135, 1)',
-                //             'rgba(255, 26, 126, 1)',
-                //             'rgba(8, 129, 210, 1)',
-                //             'rgba(123, 122, 200, 1)',
-                //             'rgba(127, 195, 246, 1)'
-                //         ],
-                //         borderWidth: 1
-                //     }]
             },
             options: {
                 scales: {
@@ -65,17 +23,13 @@
 
     $(function () {
         montarGrafico();
-
-
     });
 
     function montarGrafico() {
 
         let promises = [];
-        promises.push(countDados());
-        promises.push(countDados());
-        promises.push(countDados());
-        promises.push(countDados());
+        promises.push(countQuantidadeVeiculosCompradosMes());
+        promises.push(countQuantidadeVeiculosVendidosMes());
 
         Promise.all(promises)
             .then((dataSets) => gerarGrafico(dataSets))
@@ -83,43 +37,20 @@
 
     }
 
-
-    function countDados() {
+    function countQuantidadeVeiculosCompradosMes() {
         return new Promise((onSucces, onError) => {
-            $.get({url: "../veiculos/countQuantidadeVeiculosMes", contentType: 'application/x-www-form-urlencoded; charset=utf8'})
+            $.get({url: "../veiculos/countQuantidadeVeiculosCompradosMes", contentType: 'application/x-www-form-urlencoded; charset=utf8'})
             .then(onSucces)
             .catch(onError);
         })
     }
 
-    function getRandomColor(usedColors) {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        if (usedColors.indexOf(color) > -1) {
-            color =  getRandomColor(usedColors);
-        }
-        usedColors.push(color);
-        return color;
-    }
-    function getRamdomRGBA(usedRgbas, opacity) {
-        let o = Math.round, r = Math.random, s = 255;
-        let opcaityUsed = null;
-        if (opacity) {
-            opacityUsed = opacity;
-        } else {
-            opacityUsed = r().toFixed(1);
-            opacityUsed = opacityUsed === '0.0' ? '0.3' : opacityUsed;
-        }
-
-        let rgba = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + opacityUsed + ')';
-        if (usedRgbas.indexOf(rgba) > -1) {
-            rgba =  getRamdomRGBA(usedRgbas);
-        }
-        usedRgbas.push(rgba);
-        return rgba;
+    function countQuantidadeVeiculosVendidosMes() {
+        return new Promise((onSucces, onError) => {
+            $.get({url: "../veiculos/countQuantidadeVeiculosVendidosMes", contentType: 'application/x-www-form-urlencoded; charset=utf8'})
+            .then(onSucces)
+            .catch(onError);
+        })
     }
 
 })();
