a5bb6822bbbe7260c082efd9721f19eb1cc3c647
Vítor Marçal <git@vitormarcal.com.br>
2018-02-03 17:28:59 -0200

    colocado definicao padrão da cor no backend

--- src/main/java/com/example/demo/DataSetsController.java
+++ src/main/java/com/example/demo/DataSetsController.java
@@ -11,27 +11,38 @@ import java.util.Random;
 @RestController()
 @RequestMapping(value = "/veiculos")
 public class DataSetsController {
-    private final HashMap<Integer, Integer> resultadoBanco = new HashMap<>();
-
 
     @GetMapping("/countQuantidadeVeiculosMes")
     public DataSetDTO countQuantidadeVeiculosMes() {
 
-        gerarValores();
+        final Map<Integer, Integer> resultadoBanco = gerarValores();
         String label = "Veiculos";
         DataSetDTO dataSetDTO = new DataSetDTO(label, 12);
         for (Map.Entry<Integer, Integer> pair : resultadoBanco.entrySet()){
                 dataSetDTO.set(pair.getKey(), pair.getValue());
         }
+        dataSetDTO.setBackgroundColor(0, "rgba(255, 99, 132, 0.2)");
+        dataSetDTO.setBackgroundColor(1, "rgba(54, 162, 235, 0.2)");
+        dataSetDTO.setBackgroundColor(2, "rgba(255, 206, 86, 0.2)");
+        dataSetDTO.setBackgroundColor(3, "rgba(75, 192, 192, 0.2)");
+        dataSetDTO.setBackgroundColor(4, "rgba(153, 102, 255, 0.2)");
+        dataSetDTO.setBackgroundColor(5, "rgba(255, 159, 64, 0.2)");
+
+        dataSetDTO.setBorderColor(0, "rgba(255, 99, 132, 1)");
+        dataSetDTO.setBorderColor(1, "rgba(54, 162, 235, 1)");
+        dataSetDTO.setBorderColor(2, "rgba(255, 206, 86, 1)");
+        dataSetDTO.setBorderColor(3, "rgba(75, 192, 192, 1)");
+        dataSetDTO.setBorderColor(4, "rgba(153, 102, 255, 1)");
+        dataSetDTO.setBorderColor(5, "rgba(255, 159, 64, 1)");
         return dataSetDTO;
     }
 
     private Map<Integer, Integer> gerarValores(){
         int scale = 12;
-
+        Map<Integer, Integer> resultadoBanco = new HashMap<>();
         Random randomGenerator = new Random();
         for (int i = 1; i <= scale; i++) {
-            resultadoBanco.put(i, randomGenerator.nextInt(100));
+            resultadoBanco.put(i, randomGenerator.nextInt(10000));
         }
 
         return resultadoBanco;
